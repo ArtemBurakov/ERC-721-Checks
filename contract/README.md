f52f10d7f9eb8dd3ff9266f1bee7918a303bdf3a
Artem Burakov <artem000.burakov@gmail.com>
2023-04-05 13:29:34 +0300

    CONTRACT: Added README.md file and deployed contract on Sepolia testnet.

---
+++ contract/README.md
@@ -0,0 +1,3 @@
+# ERC-721
+
+A smart contract to create an ERC-721 Non-Fungible Token standard for product trading and more.
--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -9,13 +9,13 @@ const { PROVIDER_KEY, ACCOUNT_PRIVATE_KEY } = process.env;
 
 module.exports = {
   solidity: "0.8.1",
-  //defaultNetwork: "sepolia",
+  defaultNetwork: "sepolia",
   networks: {
     hardhat: {},
-    // sepolia: {
-    //   url: `https://sepolia.infura.io/v3/${PROVIDER_KEY}`,
-    //   accounts: [`0x${ACCOUNT_PRIVATE_KEY}`],
-    // },
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${PROVIDER_KEY}`,
+      accounts: [`0x${ACCOUNT_PRIVATE_KEY}`],
+    },
     ethereum: {
       chainId: 1,
       url: `https://mainnet.infura.io/v3/${PROVIDER_KEY}`,
